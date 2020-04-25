aa9a9ab66e58be03903a83e70ff11b2240e6a603
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-25 16:27:17 -0400

    lesson-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { LessonModule } from './lesson/lesson.module';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url:'mongodb+srv://samuel:Sam13792805@cluster0-leqzf.mongodb.net/nest?retryWrites=true&w=majority',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities:[
+        Lesson
+      ]
+
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    }),
+    LessonModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/lesson/lesson.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { LessonService } from './lesson.service';
+import { LessonResolver } from './lesson.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+
+@Module({
+    imports:[
+        TypeOrmModule.forFeature([
+            Lesson
+        ])
+    ],
+    providers:[
+        LessonResolver,
+        LessonService
+    ]
+})
+export class LessonModule {}
